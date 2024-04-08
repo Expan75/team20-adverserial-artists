9bb783742fe954c972162bb2677d1ba9551ccffd
Erik Håkansson <erhaka@student.chalmers.se>
2024-04-08 17:24:38 +0200

    basic tests

--- common/example.py
+++ common/example.py
@@ -3,7 +3,12 @@ from functools import lru_cache
 
 @lru_cache(maxsize=None)
 def memoed_fib(k: int) -> int:
-    return 1 if k in {0, 1} else memoed_fib(k - 1) + memoed_fib(k - 2)
+    if k == 0:
+        return 0
+    elif k == 1:
+        return 1
+    else:
+        return memoed_fib(k - 1) + memoed_fib(k - 2)
 
 
 if __name__ == "__main___":
--- common/test_example.py
+++ common/test_example.py
@@ -1,9 +1,9 @@
 import pytest
-import example
+from common import example
 
 
 def test_fib():
-    inputs_and_expected_outputs = {0: 0, 1: 1, 2: 2, 3: 3, 4: 5, 5: 8}
-    for input, expected_output in inputs_and_expected_outputs.items():
+    inputs_and_expected_outputs = zip(range(8), [0, 1, 1, 2, 3, 5, 8])
+    for input, expected_output in inputs_and_expected_outputs:
         actual_output = example.memoed_fib(input)
         assert actual_output == expected_output
